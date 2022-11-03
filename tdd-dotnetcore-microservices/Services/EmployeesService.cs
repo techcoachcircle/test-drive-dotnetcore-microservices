a898b0d403b7653cc91938e2c3018c2ec924d3f8
Kamal Raj Sekar <notify.kamalraj@gmail.com>
2022-11-03 11:54:57 +0000

    06_passing_service_test_with_mock_repository

--- tdd-dotnetcore-microservices/Services/EmployeesService.cs
+++ tdd-dotnetcore-microservices/Services/EmployeesService.cs
@@ -7,14 +7,15 @@ namespace tdd_dotnetcore_microservices.Services
 {
     public class EmployeesService : IEmployeesService
     {
+        private readonly IEmployeesRepository _employeeRepository;
         public EmployeesService(IEmployeesRepository employeesRepository)
         {
-
+            this._employeeRepository = employeesRepository;
         }
 
         public IEnumerable<Employee> GetAllEmployees()
         {
-            return null;
+            return _employeeRepository.GetAllEmployees();
         }
     }
 }
